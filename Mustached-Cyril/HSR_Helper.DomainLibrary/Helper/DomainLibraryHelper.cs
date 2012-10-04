9c2904bf9824c49509c32e6747ead7ddd5e47c03
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-04 09:21:04 +0200

    adding restsharp library

--- Mustached-Cyril/HSR_Helper.DomainLibrary/Helper/DomainLibraryHelper.cs
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Helper/DomainLibraryHelper.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using HSR_Helper.DomainLibrary.Domain;
+using System.ComponentModel;
 
 namespace HSR_Helper.DomainLibrary.Helper
 {
+    public delegate void BadgeInformationCallback(BadgeInformation badgeInformation);
+    public delegate void TimetableCallback(Timetable timetable);
+    public delegate void LunchtableCallback(Lunchtable lunchtable);
+
     public static class DomainLibraryHelper
     {
-        public static BadgeInformation GetUserBadgeInformation(UserInformation userInformation)
+        
+        public static void GetUserBadgeInformation(UserInformation userInformation, BadgeInformationCallback callback)
         {
-            return null;
+            
         }
 
-        public static Timetable GetUserTimetable(UserInformation userInformation)
+        public static void GetUserTimetable(UserInformation userInformation, TimetableCallback callback)
         {
-            return null;
+            
         }
 
-        public static Lunchtable GetLunchtable()
+        public static void GetLunchtable(LunchtableCallback callback)
         {
-            return null;
+            var b = new BackgroundWorker();
+            b.DoWork += (sender, args) =>
+                            {
+                                var lunchtable = new Lunchtable();
+
+                                callback(lunchtable);
+                            };
+            b.RunWorkerAsync();
         }
     }
 }
---
+++ Mustached-Cyril/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HSR_Helper.DomainLibrary.Domain;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HSR_Helper.DomainLibrary.Helper.DomainLibraryHelper.GetLunchtable(Cb);
+        }
+
+        private void Cb(Lunchtable lunchtable)
+        {
+            Console.WriteLine(lunchtable.ToString());
+        }
+    }
+}
