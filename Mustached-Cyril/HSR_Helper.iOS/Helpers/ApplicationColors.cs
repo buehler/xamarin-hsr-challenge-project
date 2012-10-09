41964642af5699c838db757ccf55297663f103d4
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-09 14:25:53 +0200

    renamed font color to table font color (white)

--- Mustached-Cyril/HSR_Helper.iOS/Helpers/ApplicationColors.cs
+++ Mustached-Cyril/HSR_Helper.iOS/Helpers/ApplicationColors.cs
@@ -7,7 +7,7 @@ namespace HSR_Helper.iOS
 	{
 		public static readonly UIColor NAVIGATIONBAR_COLOR = UIColor.FromRGB (244, 136, 0);
 		public static readonly UIColor DEFAULT_BACKGROUND_COLOR = UIColor.FromRGB (0, 95, 156);
-		public static readonly UIColor FONT_COLOR = UIColor.FromRGB (255, 255, 255);
+		public static readonly UIColor TABLE_FONT_COLOR = UIColor.FromRGB (255, 255, 255);
 	}
 }
 
