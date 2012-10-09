51b8361f05540b86ac286bc0b5b42c9431fdb2cb
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-09 23:07:07 +0200

    added application settings class (which manages settings); extended persistency class for ios

--- Mustached-Cyril/HSR_Helper.DomainLibrary.iOS/Persistency/IPhonePersistency.cs
+++ Mustached-Cyril/HSR_Helper.DomainLibrary.iOS/Persistency/IPhonePersistency.cs
@@ -1,22 +1,22 @@
 ﻿using HSR_Helper.DomainLibrary.Persistency;
 namespace HSR_Helper.DomainLibrary.iOS.Persistency
 {
-    public class IPhonePersistency : IPersistency
-    {
+	public class IPhonePersistency : IPersistency
+	{
 
-        public bool Save(IPersistentObject obj)
-        {
-            throw new System.NotImplementedException();
-        }
+		public bool Save (IPersistentObject obj)
+		{
+			throw new System.NotImplementedException ();
+		}
 
-        public bool Delete(System.Guid id)
-        {
-            throw new System.NotImplementedException();
-        }
+		public bool Delete (System.Guid id)
+		{
+			throw new System.NotImplementedException ();
+		}
 
-        public IPersistentObject Load(System.Guid id)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+		public T Load<T> (System.Guid id) where T : IPersistentObject
+		{
+			throw new System.NotImplementedException ();
+		}
+	}
 }
---
+++ Mustached-Cyril/HSR_Helper.iOS/Controller/ApplicationSettings.cs
@@ -0,0 +1,24 @@
+using System;
+using HSR_Helper.DomainLibrary.iOS.Persistency;
+
+namespace HSR_Helper.iOS
+{
+	public class ApplicationSettings
+	{
+		private static ApplicationSettings _instance;
+		public static ApplicationSettings Instance {
+			get {
+				if (_instance == null)
+					_instance = new ApplicationSettings ();
+				return _instance;
+			}
+		}
+
+		public IPhonePersistency Persistency{ get; private set; }
+
+		private ApplicationSettings ()
+		{
+		}
+	}
+}
+
