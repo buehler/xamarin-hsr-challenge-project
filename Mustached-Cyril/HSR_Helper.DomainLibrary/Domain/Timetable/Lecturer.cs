e9b1540de6ed746d83e6bd09fd041609859644f4
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-10 22:15:28 +0200

    added timetable classes

---
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Domain/Timetable/Lecturer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSR_Helper.DomainLibrary.Domain.Timetable
+{
+    public class Lecturer
+    {
+    }
+}
