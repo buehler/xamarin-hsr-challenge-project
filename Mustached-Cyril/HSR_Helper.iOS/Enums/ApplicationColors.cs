f82a29a8e8694743997a2af001da50b5f605f639
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-10 08:18:46 +0200

    moved colors to enums

--- Mustached-Cyril/HSR_Helper.iOS/Enums/ApplicationColors.cs
+++ Mustached-Cyril/HSR_Helper.iOS/Enums/ApplicationColors.cs
@@ -5,8 +5,8 @@ namespace HSR_Helper.iOS
 {
 	public static class ApplicationColors
 	{
-		public static readonly UIColor NAVIGATIONBAR_COLOR = UIColor.FromRGB (244, 136, 0);
-		public static readonly UIColor DEFAULT_BACKGROUND_COLOR = UIColor.FromRGB (0, 95, 156);
+		public static readonly UIColor NAVIGATIONBAR_COLOR = UIColor.FromRGB (255, 0, 0);//(244, 136, 0);
+		public static readonly UIColor DEFAULT_BACKGROUND_COLOR = UIColor.FromRGB (255, 0, 255);//(0, 95, 156);
 		public static readonly UIColor TABLE_HEADER_FONT_COLOR = UIColor.FromRGB (255, 255, 255);
 		public static readonly UIColor TABLE_FONT_COLOR = UIColor.FromRGB (0, 0, 0);
 		public static readonly UIColor TABLE_DETAIL_FONT_COLOR = UIColor.FromRGB (0, 95, 156);
