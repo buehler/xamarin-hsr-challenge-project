37bce36adfa91450e173c960944d5f54dde2aacc
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-10 21:41:03 +0200

    added timetableday with json properties

--- Mustached-Cyril/HSR_Helper.DomainLibrary/Domain/TimetableDay.cs
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Domain/TimetableDay.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
 
 namespace HSR_Helper.DomainLibrary.Domain
 {
     public class TimetableDay
     {
+        [JsonProperty("Description")]
+        public string Weekday { get; set; }
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+        [JsonProperty("Lessons")]
+        public List<Lession> Lessions { get; set; }
 
+        public TimetableDay()
+        {
+            Lessions = new List<Lession>();
+        }
     }
 }
