da65e503ffd75cab4efdd7e35db4102e87af119f
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-13 18:10:54 +0200

    adding multitablesupport to timetable; added prototyping support for persistency

--- Mustached-Cyril/HSR_Helper.DomainLibrary/Persistency/IPersistency.cs
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Persistency/IPersistency.cs
@@ -1,11 +1,12 @@
 
 namespace HSR_Helper.DomainLibrary.Persistency
 {
-	public interface IPersistency
-	{
-		bool Save (PersistentObject obj);
-	    bool Exists<T>() where T : PersistentObject, new();
+    public interface IPersistency
+    {
+        bool Save(PersistentObject obj);
+        bool Exists<T>() where T : PersistentObject, new();
         bool Delete<T>() where T : PersistentObject, new();
         T Load<T>() where T : PersistentObject, new();
-	}
+        T Load<T>(T prototype) where T : PersistentObject, new();
+    }
 }
