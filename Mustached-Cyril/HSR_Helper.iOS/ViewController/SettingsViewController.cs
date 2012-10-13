1f04bff61a31db97dec2a630b48ab7509674e760
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-13 17:57:26 +0200

    building multiple timetables

--- Mustached-Cyril/HSR_Helper.iOS/ViewController/SettingsViewController.cs
+++ Mustached-Cyril/HSR_Helper.iOS/ViewController/SettingsViewController.cs
@@ -2,16 +2,17 @@ using System;
 using MonoTouch.Dialog;
 using HSR_Helper.DomainLibrary.Domain.Timetable;
 using HSR_Helper.DomainLibrary.Domain.Lunchtable;
+using MonoTouch.UIKit;
 
 namespace HSR_Helper.iOS
 {
 	public class SettingsViewController : DefaultDialogViewController
 	{
-		public SettingsViewController () : base(new RootElement("Einstellungen"))
+		public SettingsViewController() : base(new RootElement("Einstellungen"))
 		{
-			var userEntry = new EntryElement ("Benutzername", "benutzername", ApplicationSettings.Instance.UserCredentials.Name);
-			var passwordEntry = new EntryElement ("Passwort", "passwort", ApplicationSettings.Instance.UserCredentials.Name, true);
-			var deleteLocalFiles = new StyledStringElement ("Lokale Daten löschen", DeleteLocalFiles){
+			var userEntry = new EntryElement("Benutzername", "benutzername", ApplicationSettings.Instance.UserCredentials.Name);
+			var passwordEntry = new EntryElement("Passwort", "passwort", ApplicationSettings.Instance.UserCredentials.Name, true);
+			var deleteLocalFiles = new StyledStringElement("Lokale Daten löschen", DeleteLocalFiles){
 				BackgroundColor = ApplicationColors.DANGER_BUTTON,
 				TextColor = MonoTouch.UIKit.UIColor.White
 			};
@@ -19,43 +20,60 @@ namespace HSR_Helper.iOS
 			userEntry.AutocapitalizationType = passwordEntry.AutocapitalizationType = MonoTouch.UIKit.UITextAutocapitalizationType.None;
 			userEntry.Changed += UsernameChanged;
 			passwordEntry.Changed += PasswordChanged;
-			Root.Add (new Section ("Benutzerinformationen"){
+
+			Root.Add(new Section("Benutzerinformationen"){
 				userEntry,
 				passwordEntry
 			});
-			Root.Add (new Section ("Daten löschen"){
+
+			Root.Add(new Section("Stundenplaneinstellungen"){
+				new StyledStringElement("Andere Stundenpläne", ShowOtherTimetables){
+					Accessory = UITableViewCellAccessory.DisclosureIndicator
+				}
+			});
+
+			Root.Add(new Section("Daten löschen"){
 				deleteLocalFiles
 			});
+
 			Title = "Einstellungen";
 			NavigationItem.Title = "Einstellungen";
 		}
 
-		private void UsernameChanged (object s, EventArgs e)
+		private void UsernameChanged(object s, EventArgs e)
 		{
-			Console.WriteLine ("Username changed");
+			Console.WriteLine("Username changed");
 			var field = s as EntryElement;
-			if (field != null) {
+			if (field != null)
+			{
 				ApplicationSettings.Instance.UserCredentials.Name = field.Value;
-				ApplicationSettings.Instance.Persistency.Save (ApplicationSettings.Instance.UserCredentials);
+				ApplicationSettings.Instance.Persistency.Save(ApplicationSettings.Instance.UserCredentials);
 			}
 		}
 
-		private void PasswordChanged (object s, EventArgs e)
+		private void PasswordChanged(object s, EventArgs e)
 		{
-			Console.WriteLine ("PW Changed");
+			Console.WriteLine("PW Changed");
 			var field = s as EntryElement;
-			if (field != null) {
+			if (field != null)
+			{
 				ApplicationSettings.Instance.UserCredentials.Password = field.Value;
-				ApplicationSettings.Instance.Persistency.Save (ApplicationSettings.Instance.UserCredentials);
+				ApplicationSettings.Instance.Persistency.Save(ApplicationSettings.Instance.UserCredentials);
 			}
 		}
 
-		private void DeleteLocalFiles ()
+		private void ShowOtherTimetables()
 		{
-			Console.WriteLine ("Delete Local Files!!");
-//			ApplicationSettings.Instance.UserCredentials.Name = null;
-//			ApplicationSettings.Instance.UserCredentials.Password = null;
-			ApplicationSettings.Instance.Persistency.Delete<Timetable> ();
+			var dvc = new TimetableViewController();
+			NavigationController.PushViewController(dvc, true);
+		}
+
+		private void DeleteLocalFiles()
+		{
+			Console.WriteLine("Delete Local Files!!");
+			ApplicationSettings.Instance.UserCredentials.Name = null;
+			ApplicationSettings.Instance.UserCredentials.Password = null;
+			ApplicationSettings.Instance.Persistency.Delete<Timetable>();
 //			ApplicationSettings.Instance.Persistency.Delete<Lunchtable> ();
 		}
 	}
