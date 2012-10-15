3929535efe39a9ec02541764ef8b19731b259081
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-15 09:47:10 +0200

    changing

--- Mustached-Cyril/HSR_Helper.DomainLibrary/Domain/Userinformation/UserCredentials.cs
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Domain/Userinformation/UserCredentials.cs
@@ -4,35 +4,43 @@ using HSR_Helper.DomainLibrary.Security;
 
 namespace HSR_Helper.DomainLibrary.Domain.Userinformation
 {
-	public sealed class UserCredentials : SecureObject
-	{
-		private string _name;
-		public string Name {
-			get {
-				return _name;
-			}
-			set {
-				_name = value;
-				OnObjectChanged ();
-			}
-		}
+    public sealed class UserCredentials : SecureObject
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnObjectChanged();
+            }
+        }
 
-		private string _password;
-		public string Password {
-			get {
-				return _password;
-			}
-			set {
-				_password = value;
-				OnObjectChanged ();
-			}
-		}
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnObjectChanged();
+            }
+        }
 
-		[XmlIgnore]
-		public bool CredentialsFilled {
-			get {
-				return !string.IsNullOrEmpty (Name) && !string.IsNullOrEmpty (Password);
-			}
-		}
-	}
+        [XmlIgnore]
+        public bool CredentialsFilled
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Password);
+            }
+        }
+    }
 }
