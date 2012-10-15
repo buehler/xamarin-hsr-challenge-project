5a64e38bb383a7f169b8c5bce5dc90253d086a82
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-15 09:11:01 +0200

    added different sln files for win and osx; added event driven own timetable

--- Mustached-Cyril/HSR_Helper.iOS/ViewController/TimetableMasterViewController.cs
+++ Mustached-Cyril/HSR_Helper.iOS/ViewController/TimetableMasterViewController.cs
@@ -4,58 +4,65 @@ using MonoTouch.Dialog;
 using System.Collections.Generic;
 using MonoTouch.UIKit;
 using HSR_Helper.DomainLibrary.Persistency;
+using HSR_Helper.DomainLibrary.Domain.Userinformation;
 
 namespace HSR_Helper.iOS
 {
-	public class TimetableMasterViewController : DefaultDialogViewController
-	{
-		private ApplicationSettings.UserTimetableList _userList;
+    public class TimetableMasterViewController : DefaultDialogViewController
+    {
+        private UserTimetableList _userList;
+        private StyledStringElement _ownTimetable;
+        private Section _otherTimetables;
 
-		public TimetableMasterViewController() : base(new RootElement("Stundenpläne"))
-		{
-			Title = "Stundenpläne";
-			NavigationItem.Title = "Stundenpläne";
-			TabBarItem.Image = UIImage.FromBundle("Timetable-icon");
-			ApplicationSettings.Instance.UserCredentials.ObjectChanged += PersistentObjectChanged;
-		}
+        public TimetableMasterViewController() : base(new RootElement("Stundenpläne"))
+        {
+            Title = "Stundenpläne";
+            NavigationItem.Title = "Stundenpläne";
+            TabBarItem.Image = UIImage.FromBundle("Timetable-icon");
+            ApplicationSettings.Instance.UserCredentials.ObjectChanged += PersistentObjectChanged;
+        }
 
-		public override void ViewDidAppear(bool animated)
-		{
-			base.ViewDidAppear(animated);
-			if (_userList == null || !_userList.Equals(ApplicationSettings.Instance.UserTimetablelist))
-			{
-			
-				Root.Clear();
-				Root.Add(new Section("Eigener Stundenplan"){
-				new StyledStringElement(ApplicationSettings.Instance.UserCredentials.Name, () => {ButtonClicked(ApplicationSettings.Instance.UserCredentials.Name);}){
-					Accessory = MonoTouch.UIKit.UITableViewCellAccessory.DisclosureIndicator
-				}
-			});
-				var timetables = new Section("Andere Stundenpläne");
-				foreach (var username in ApplicationSettings.Instance.UserTimetablelist.Usernames)
-				{
-					timetables.Add(new StyledStringElement(username, () => {
-						ButtonClicked(username);}){
-						Accessory = MonoTouch.UIKit.UITableViewCellAccessory.DisclosureIndicator
-					});
-				}
-				Root.Add(timetables);
-				_userList = new ApplicationSettings.UserTimetableList(){Usernames = new HashSet<string>(ApplicationSettings.Instance.UserTimetablelist.Usernames)};
-			}
-		}
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            PersistentObjectChanged(ApplicationSettings.Instance.UserCredentials);
+            PersistentObjectChanged(ApplicationSettings.Instance.UserTimetablelist);
+        }
 
-		private void PersistentObjectChanged(PersistentObject obj)
-		{
-			if (IsViewLoaded)
-			{
+        private void PersistentObjectChanged(PersistentObject obj)
+        {
+            if (IsViewLoaded)
+            {
+                if (obj.GetType() == typeof(UserCredentials))
+                {
+                    if (_ownTimetable == null)
+                    {
+                        _ownTimetable = new StyledStringElement(ApplicationSettings.Instance.UserCredentials.Name, OnTapped){
+                            Accessory = MonoTouch.UIKit.UITableViewCellAccessory.DisclosureIndicator
+                        };
+                        Root.Add(new Section("Eigener Stundenplan"){_ownTimetable});
+                    }
+                    else
+                    {
+                        _ownTimetable.Caption = ApplicationSettings.Instance.UserCredentials.Name;
+                    }
+                }
+                else if (obj.GetType() == typeof(UserTimetableList))
+                {
+                    if (_otherTimetables == null)
+                    {
+                        _otherTimetables = new Section("Andere Stundenpläne");
+                        Root.Add(_otherTimetables);
+                    }
+                }
+                this.ReloadData();
+            }
+        }
 
-			}
-		}
-
-		private void ButtonClicked(string username)
-		{
-			NavigationController.PushViewController(new TimetableViewController(username), true);
-		}
-	}
+        private void OnTapped()
+        {
+            NavigationController.PushViewController(new TimetableViewController(ApplicationSettings.Instance.UserCredentials.Name), true);
+        }
+    }
 }
 
