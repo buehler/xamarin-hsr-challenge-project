d32cd3a4c7fd9a5c7f651720ca86c75cf8887f17
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-15 08:42:30 +0200

    adding usertimetable list

---
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Domain/Userinformation/UserTimetableList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HSR_Helper.DomainLibrary.Persistency;
+
+namespace HSR_Helper.DomainLibrary.Domain.Userinformation
+{
+    public class UserTimetableList : PersistentObject
+    {
+        public HashSet<string> Usernames { get; set; }
+
+        public string LastOpenedTimetable { get; set; }
+
+        public UserTimetableList()
+        {
+            Usernames = new HashSet<string>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as UserTimetableList;
+            return o != null && Usernames.SetEquals(o.Usernames);
+        }
+
+        public override int GetHashCode()
+        {
+            var o = Usernames.Aggregate("", (current, s) => current + s);
+            return o.GetHashCode();
+        }
+    }
+}
