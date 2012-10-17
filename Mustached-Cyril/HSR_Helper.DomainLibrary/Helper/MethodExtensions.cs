7dbd3db60f57d6e7e5bd4709d5f8919c0eb9a868
Christoph Buehler <christoph.buehler@hsr.ch>
2012-10-17 22:17:08 +0200

    added different symbols; modified functions for observable collections

--- Mustached-Cyril/HSR_Helper.DomainLibrary/Helper/MethodExtensions.cs
+++ Mustached-Cyril/HSR_Helper.DomainLibrary/Helper/MethodExtensions.cs
@@ -6,17 +6,19 @@ using HSR_Helper.DomainLibrary.Helper;
 
 namespace HSR_Helper.DomainLibrary.Helper
 {
-    public static class ObservableCollectionExtensions
-    {
-        public static bool ContentsAreIdentical<T>(this ObservableCollection<T> items, ObservableCollection<T> otherItems)
-        {
-            var l1 = from o in items
-                     where !otherItems.Contains(o)
+	public static class ObservableCollectionExtensions
+	{
+		public static bool ContentsAreIdentical<T>(this ObservableCollection<T> items, ObservableCollection<T> otherItems)
+		{
+			if (items.Count() != otherItems.Count())
+				return false;
+			var l1 = from o in items
+                     where !otherItems.Any(i => o.Equals(i))
                      select o;
-            var l2 = from o in otherItems
-                     where !items.Contains(o)
+			var l2 = from o in otherItems
+					 where !items.Any(i => o.Equals(i))
                      select o;
-            return (l1.Count() == 0 && l2.Count() == 0);
-        }
-    }
+			return (l1.Count() == 0 && l2.Count() == 0);
+		}
+	}
 }
