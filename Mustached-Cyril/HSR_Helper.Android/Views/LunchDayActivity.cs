968bc0f19616a57d661d9260bed5370f5a66c26b
Lorenz Bösch <lboesch@hsr.ch>
2012-11-11 19:53:28 +0100

    Added Callback for Timetable

--- Mustached-Cyril/HSR_Helper.Android/Views/LunchDayActivity.cs
+++ Mustached-Cyril/HSR_Helper.Android/Views/LunchDayActivity.cs
@@ -15,6 +15,7 @@ using HSR_Helper.DomainLibrary.Domain.Lunchtable;
 using Cheesebaron.HorizontalPager;
 using HSR_Helper.Android.Views;
 using HSR_Helper.Andoid;
+using System.Globalization;
 
 namespace HSR_Helper.Android
 {
@@ -34,16 +35,25 @@ namespace HSR_Helper.Android
             test.Text = "Am Lade...";
             horiPager.AddView(test);
             var _loadedLuchtable = ApplicationSettings.Instance.Persistency.Load<Lunchtable>();
-            if (_loadedLuchtable == null || _loadedLuchtable.LunchDays.Count <1)
+            if (_loadedLuchtable != null && _loadedLuchtable.LunchDays.Count > 0)
             {
-                DomainLibraryHelper.GetLunchtable(lunchtableCallback);
-                
+                var cal = CultureInfo.CurrentCulture.Calendar;
+                int db = cal.GetWeekOfYear(_loadedLuchtable.LastUpdated, DateTimeFormatInfo.CurrentInfo.CalendarWeekRule,
+                DateTimeFormatInfo.CurrentInfo.FirstDayOfWeek);
+                System.Console.WriteLine("Kalender woche des Ladens: " + db);
+                int act = cal.GetWeekOfYear(System.DateTime.Today, DateTimeFormatInfo.CurrentInfo.CalendarWeekRule,
+                DateTimeFormatInfo.CurrentInfo.FirstDayOfWeek);
+                System.Console.WriteLine("Kalender woche aktuell: " + act);
+
+                if (act <= db)
+                {
+                    DomainLibraryHelper.GetLunchtable(lunchtableCallback);
+                    return;
+                }
             }
-            else
-            {
-                System.Console.WriteLine(_loadedLuchtable.LastUpdated);
-                addLunchtable(_loadedLuchtable);
-            }
+            System.Console.WriteLine(_loadedLuchtable.LastUpdated);
+            addLunchtable(_loadedLuchtable);
+
         }
 
         public void lunchtableCallback(Lunchtable lunchtable)
@@ -64,10 +74,18 @@ namespace HSR_Helper.Android
             if (day_value > 5 || day_value < 0) day_value = 0;
             foreach (LunchDay day in lunchtable.LunchDays)
             {
-                System.Console.WriteLine(day.DateString);
                 this.RunOnUiThread(() => horiPager.AddView(new LunchDayView(day).GetView(this, day_value)));
             }
             this.RunOnUiThread(() => horiPager.SetCurrentScreen(day_value, false));
+            System.Console.WriteLine(lunchtable.LastUpdated);
+        }
+
+        int calculateWeekOfYear(DateTime date)
+        {
+            var cal = CultureInfo.CurrentCulture.Calendar;
+            return cal.GetWeekOfYear(date, 
+                DateTimeFormatInfo.CurrentInfo.CalendarWeekRule,
+                DateTimeFormatInfo.CurrentInfo.FirstDayOfWeek);
         }
 
     }
--- Mustached-Cyril/HSR_Helper.Android/Views/TimetableActivity.cs
+++ Mustached-Cyril/HSR_Helper.Android/Views/TimetableActivity.cs
@@ -11,6 +11,8 @@ using Android.Views;
 using Android.Widget;
 using Cheesebaron.HorizontalPager;
 using HSR_Helper.DomainLibrary.Helper;
+using HSR_Helper.Andoid;
+using HSR_Helper.DomainLibrary.Domain.Timetable;
 
 namespace HSR_Helper.Android
 {
@@ -26,7 +28,23 @@ namespace HSR_Helper.Android
             var display = WindowManager.DefaultDisplay;
             horiPager = new HorizontalPager(this.ApplicationContext, display);
             SetContentView(horiPager);
-            //DomainLibraryHelper.GetUserTimetable();
+            DomainLibraryHelper.GetUserTimetable(ApplicationSettings.Instance.UserCredentials,TimetableCallback);
+        }
+
+        private void TimetableCallback(DomainLibrary.Domain.Timetable.Timetable timetable, object[] callbackArguments)
+        {
+            if (timetable != null)
+            {
+                foreach (TimetableDay day in timetable.TimetableDays) 
+                {
+                    System.Console.WriteLine(day.Weekday);
+                    foreach (Lession lession in day.Lessions)
+                    {
+                        System.Console.WriteLine(lession.Name);
+                    }
+
+                }
+            }
         }
 
     }
